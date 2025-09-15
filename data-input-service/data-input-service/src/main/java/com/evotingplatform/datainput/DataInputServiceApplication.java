d4ed24629fd805ffc762858cfa8051ea21eedb29
Bollam Bharath <bollambharath99@gmail.com>
2025-09-15 16:59:07 +0530

    setup added

--- data-input-service/data-input-service/src/main/java/com/evotingplatform/datainput/DataInputServiceApplication.java
+++ data-input-service/data-input-service/src/main/java/com/evotingplatform/datainput/DataInputServiceApplication.java
@@ -3,11 +3,26 @@ package com.evotingplatform.datainput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.lang.System.*;
+
 @SpringBootApplication
 public class DataInputServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataInputServiceApplication.class, args);
+		printAppName();
+		out.println("Application Running at: http://localhost:1313");
+	}
+
+	private static void printAppName() {
+		out.println();
+		out.println("██████╗  █████╗ ████████╗ █████╗     ██╗███╗   ██╗██████╗ ██╗   ██╗████████╗");
+		out.println("██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗    ██║████╗  ██║██╔══██╗██║   ██║╚══██╔══╝");
+		out.println("██║  ██║███████║   ██║   ███████║    ██║██╔██╗ ██║██████╔╝██║   ██║   ██║   ");
+		out.println("██║  ██║██╔══██║   ██║   ██╔══██║    ██║██║╚██╗██║██╔═══╝ ██║   ██║   ██║   ");
+		out.println("██████╔╝██║  ██║   ██║   ██║  ██║    ██║██║ ╚████║██║     ╚██████╔╝   ██║   ");
+		out.println("╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝    ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝    ╚═╝   ");
+		out.println();
 	}
 
 }
