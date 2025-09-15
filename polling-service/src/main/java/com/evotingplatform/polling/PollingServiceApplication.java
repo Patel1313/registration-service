0d8c11abcbc4d41d895b614435e932f7af2932dd
Bollam Bharath <bollambharath99@gmail.com>
2025-09-15 15:50:44 +0530

    Initial commit - data-input-service

---
+++ polling-service/src/main/java/com/evotingplatform/polling/PollingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evotingplatform.polling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingServiceApplication.class, args);
+	}
+
+}
---
+++ polling-service/src/test/java/com/evotingplatform/polling/PollingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evotingplatform.polling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
